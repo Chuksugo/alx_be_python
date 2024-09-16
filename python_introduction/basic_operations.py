3bc8fc510a9a9cfda2738a0e37ec65eb3c99822a
Chuksugo <rhemekas.inc@gmail.com>
2024-09-16 03:41:24 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define two variables with specific values
+number1 = 10
+number2 = 5
+
+# Perform calculations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results in human-readable format
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
